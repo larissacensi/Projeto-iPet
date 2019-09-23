e90df2d8d1f78839ed8b06bafb8aba951c6ff7d2
Alex Freitag <alex.f@quantich.com.br>
2019-09-23 20:14:24 -0300

    inclusão de flatlist na tela de adoção

--- src/screens/Adocao.js
+++ src/screens/Adocao.js
@@ -6,7 +6,7 @@ import {
   TouchableHighlight,
   Dimensions,
   Image,
-  Keyboard,
+  FlatList,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -21,15 +21,38 @@ export default class Home extends Component {
       </View>
   });
 
+  state = {
+    data: [
+      {id: '1', name: 'Luna', age: 2, time: 'anos', description: 'bla bla bla bla'},
+      {id: '2', name: 'Laila', age: 5, time: 'meses', description: 'bla bla bla bla'},
+      {id: '3', name: 'Rambo', age: 6, time: 'meses', description: 'bla bla bla bla'},
+    ]
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 2, justifyContent: 'flex-start', marginTop: 20}}>
+        <View style={{justifyContent: 'flex-start', alignItems:'center', marginTop: 20, marginBottom: 30}}>
           <Text style={styles.title}>Adotar</Text>
         </View>
-        <View style={styles.list} >
-          <Text>PUTA QUE PARIU BICHO</Text>
-        </View>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.item}>
+                <View style={styles.viewImage}>
+                  <Text style={{color: 'black'}}>Imagem do animal aqui</Text>
+                </View>
+                <View style={{/*backgroundColor: 'green',*/ justifyContent: 'center', marginLeft: 10}}>
+                  <Text style={styles.text}>{item.name}</Text>
+                  <Text style={styles.text}>{item.age} {item.time}</Text>
+                  <Text style={{fontSize: 18}}>{item.description}</Text>
+                </View>
+              </View>
+            );
+          }}
+        />
     </View>
     );
   }
@@ -40,35 +63,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     //justifyContent: 'center',
-    alignItems: 'center',
+    //alignItems: 'center',
   },
   text: {
-    color: 'white',
-    fontSize: 24,
-    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 20,
   },
   title: {
     color: '#2fb7a7',
     fontSize: 54,
     fontFamily: 'GROBOLD',
   },
-  button: {
-    backgroundColor: '#2fb7a7',
-    padding: 10,
-    borderRadius: 5,
-    margin: 10,
-    width: Dimensions.get('window').width / 1.5,
-    height: Dimensions.get('window').height / 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   viewImage: {
-    flex: 3,
-    //padding: 30,
-    //marginBottom: 50
+    height: 100,
+    width: 100,
+    borderColor: 'purple',
+    borderWidth: 3,
+    justifyContent: 'center',
+    borderRadius: 20
   },
-  list: {
-    flex: 2,
-    backgroundColor: 'blue'
+  item: {
+    alignItems: "center",
+    //backgroundColor: "#dcda48",
+    flexGrow: 1,
+    margin: 4,
+    padding: 20,
+    flexDirection: 'row',
+    borderBottomColor: 'grey',
+    borderBottomWidth: 2
   },
 });
